657de977cbbe1a00be74f48e9e0c9db44d6bcebd
Tonter <45268315+Tonter@users.noreply.github.com>
2021-03-17 22:54:18 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,124 @@
+import os
+import openpyxl
+from openpyxl.styles import Color, Alignment, PatternFill, Font, Border, Side
+
+#Changes directory
+os.chdir(os.getcwd())
+
+#loads an excel sheet
+wb  = openpyxl.load_workbook("rl_trades.xlsx")
+
+#Chooses a sheet
+mixSheet = wb['All']
+
+#Styles
+blueFill = PatternFill(start_color='89D8F5',
+                   end_color='89D8F5',
+                   fill_type='solid')
+
+redFill = PatternFill(start_color='FF8295',
+                   end_color='FF8295',
+                   fill_type='solid')
+
+solidRedFill = PatternFill(start_color='e02f2f',
+                   end_color='e02f2f',
+                   fill_type='solid')
+
+solidGreenFill = PatternFill(start_color='2fe084',
+                   end_color='2fe084',
+                   fill_type='solid')
+
+thin_border = Border(left=Side(style='thin'), 
+                     right=Side(style='thin'), 
+                     top=Side(style='thin'), 
+                     bottom=Side(style='thin'))
+
+
+def save():
+    os.chdir(os.getcwd())
+    wb.save('rl_trades.xlsx')
+
+def addTrade():
+    #Input
+    traderName = input("Name: ")
+    BuyOrSell = input("Buy or sell: ")
+    offer1 = input("What item/offer: ")
+    offer2 = input("For: ")
+    friend = input("Friends?: ")
+
+
+
+    #Setup
+    currentRow = mixSheet.max_row + 1
+    maxColumn = mixSheet.max_column + 1 
+    BuyOrSell = BuyOrSell.upper()
+
+    #Style
+    if BuyOrSell == "S" or BuyOrSell == "Sell" or BuyOrSell == "sell":
+        for i in range(1, maxColumn):
+            mixSheet.cell(row=currentRow, column=i).fill = redFill
+            mixSheet.cell(row=currentRow, column=i).border = thin_border
+    elif BuyOrSell == "B" or BuyOrSell == "Buy" or BuyOrSell == "buy":
+        for i in range(1, maxColumn):
+            mixSheet.cell(row=currentRow, column=i).fill = blueFill
+            mixSheet.cell(row=currentRow, column=i).border = thin_border
+
+        
+    #Trader Name
+    currentCell = mixSheet.cell(row=currentRow, column=1)
+    currentCell.value = traderName
+    currentCell.alignment = Alignment(horizontal='center')
+
+
+    #Sell Or Buy
+    currentCell = mixSheet.cell(row=currentRow, column=2)
+    currentCell.value = "[" + BuyOrSell + "]"
+    currentCell.alignment = Alignment(horizontal='center')
+
+    #Offer 1
+    currentCell = mixSheet.cell(row=currentRow, column=3)
+    currentCell.value = offer1
+    currentCell.alignment = Alignment(horizontal='center')
+
+    #For column
+    currentCell = mixSheet.cell(row=currentRow, column=4)
+    currentCell.value = "FOR"
+    currentCell.alignment = Alignment(horizontal='center')
+
+
+    #Offer 2
+    currentCell = mixSheet.cell(row=currentRow, column=5)
+    if  offer2.isdigit(): 
+       currentCell.value = offer2 + "c"
+    else:
+        currentCell.value = offer2
+    
+    currentCell.alignment = Alignment(horizontal='center')
+
+
+    #Friends with
+    currentCell = mixSheet.cell(row=currentRow, column=6)
+    if friend == "n" or friend == "no" or friend == "No":
+        currentCell.value = "NO"
+        currentCell.fill = solidRedFill
+        currentCell.alignment = Alignment(horizontal='center')
+    elif friend == "y" or friend == "yes" or friend == "Yes":
+        currentCell.value = "YES"
+        currentCell.fill = solidGreenFill
+        currentCell.alignment = Alignment(horizontal='center')
+
+
+
+    #Commits the changes (twice)
+    try:
+        save()
+    except:
+        inp = input("Couldn't save the changes, Try closing the Excel file. ")
+        if inp == "":
+            save()
+
+#Inputs
+firstInput = input("Type Enter to Start... ")
+while firstInput == "":
+    addTrade()
+    firstInput = input("\nEnter to keep going...")
